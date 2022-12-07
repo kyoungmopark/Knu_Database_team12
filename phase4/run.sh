32e86c64fd260fcf9dadd339f6ccd90eeb34a9ac
Kitae Kim <develoot@gmail.com>
2022-12-08 01:55:24 +0900

    Implement automation script

--- phase4/run.sh
+++ phase4/run.sh
@@ -18,9 +18,27 @@ docker pull container-registry.oracle.com/database/enterprise:latest
 # Note that the application server will use port 3000 by default.
 (cd ./library-manager && docker build . --build-arg NODE_ENV_ARG=production -t comp322-library-manager:latest)
 
-# Run oracle dbms.
+# Run Oracle DBMS.
 docker rm -f comp322-oracle-dbms || :
-docker run -d --network=host --name comp322-oracle-dbms container-registry.oracle.com/database/enterprise:latest
+docker run -d --network=host --env ORACLE_PWD="password" --name comp322-oracle-dbms container-registry.oracle.com/database/enterprise:latest
+until [[ "$(docker inspect -f {{.State.Health.Status}} comp322-oracle-dbms)" == "healthy" ]]; do
+	echo "Wait until the Oracle DBMS is ready. It may take a while!"
+	sleep 1;
+done
+
+# Create an Oracle DBMS user.
+echo "create user university identified by comp322;" \
+	| docker exec -i comp322-oracle-dbms sqlplus sys/password@orclpdb1 as sysdba
+echo "grant all privileges to university;" \
+	| docker exec -i comp322-oracle-dbms sqlplus sys/password@orclpdb1 as sysdba
+
+# Copy SQL scripts into the Oracle DBMS container.
+docker cp ../create_table.sql comp322-oracle-dbms:/home/oracle
+docker cp ../insert.sql comp322-oracle-dbms:/home/oracle
+
+# Execute the SQL scripts inside the Oracle DBMS container.
+echo "@create_table" | docker exec -i comp322-oracle-dbms sqlplus university/comp322@orclpdb1
+echo "@insert" | docker exec -i comp322-oracle-dbms sqlplus university/comp322@orclpdb1
 
 # Run the application server.
 docker rm -f comp322-library-manager || :
