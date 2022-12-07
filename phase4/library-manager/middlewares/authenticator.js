cde0a67c97717656dfd67ba4e73b58b7f7f00bdb
Kitae Kim <develoot@gmail.com>
2022-12-08 03:48:51 +0900

    Make buttons work

--- phase4/library-manager/middlewares/authenticator.js
+++ phase4/library-manager/middlewares/authenticator.js
@@ -1,14 +1,31 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+  if (!req.cookies.token) {
+    // return res.status(403).json({ error: 'HTTP Authorization Header is empty.' });
+    console.log('Token cookie is empty.');
+    next();
+    return;
+  }
+
+  const token = req.cookies.token;
+
+  /*
   if (!req.headers.authorization) {
-    return res.status(403).json({ error: 'HTTP Authorization Header is empty.' });
+    // return res.status(403).json({ error: 'HTTP Authorization Header is empty.' });
+    console.log('HTTP Authorization Header is empty.');
+    next();
+    return;
   }
 
   const token = req.headers.authorization.split(' ')[1];
   if (!token) {
-    return res.status(403).json({ error: 'Bearer token is empty.' });
+    // return res.status(403).json({ error: 'Bearer token is empty.' });
+    console.log('Bearer token is empty.');
+    next();
+    return;
   }
+  */
 
   let payload;
   try {
--- phase4/library-manager/routes/index.js
+++ phase4/library-manager/routes/index.js
@@ -2,17 +2,56 @@ var express = require('express');
 var router = express.Router();
 const oracledb = require('oracledb');
 let connection;
+const jwt = require('jsonwebtoken');
+const authenticator = require(__dirname + '/../middlewares/authenticator');
+const util = require('util');
 
 const userDB = "university";
 const passwordDB = "comp322";
 
-const userID = "User21";
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('view folder/jade file name');
-  res.render('index'); 
+  res.render('index');
 });
+
+router.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(503).json({ error: 'Not enough arguments.' });
+  }
+
+  let token;
+  try {
+    connection = await oracledb.getConnection({
+      user  : userDB,
+      password  : passwordDB,
+      connectionString  : 'localhost/orclpdb1'
+    });
+
+    console.log("Succesfully connected to Oracle!!");
+
+    const str = `select * from ACCOUNT where id = '${username}' and password = '${password}'`
+    const qr = await connection.execute(str)
+    if (qr.rows.length < 1) {
+      return res.status(503).json({ error: 'Failed to login.' });
+    }
+
+    token = jwt.sign({
+        username: username,
+      }, req.app.get('jwtsecret'), {
+        expiresIn: '99h',
+        issuer: '2022-knu-capstone-auth',
+    });
+  } catch (error) {
+    console.error('Error occured during login: ' + error.toString());
+    res.status(503).json({ error: 'Internal Error' });
+  }
+
+  res.cookie('token', token)
+  res.status(200).json({ message: 'Success', token: token })
+});
+
 /* book search */
 router.get('/book',(req,res, next) => {
   let searchOption = req.query.searchOption;
@@ -49,7 +88,7 @@ router.get('/book',(req,res, next) => {
         + "where book.isbn = authored.book_id and authored.author_id = author.id "
         + "and book.isbn like '%" + searchWord + "%' ";
       }
-      
+
       const qr = await connection.execute(str)
       const count = qr.rows.length;
 
@@ -129,10 +168,80 @@ router.get('/reviewAll',(req,res,next) => {
     }
   }
 
-  fun();  
+  fun();
 })
+
 /* show only my reviews */
-router.get('/reviewUser',(req,res,next) => {
+router.use('/reviewUser', authenticator);
+router.get('/reviewUser', async (req,res,next) => {
+  let str;
+  var isbn = req.query.bookISBN;
+  var title = req.query.bookTitle;
+  var rating = req.query.bookRating;
+  var comment = req.query.bookComment;
+
+  if (!req.jwtPayload || Object.entries(req.jwtPayload).length == 0) {
+    console.log(util.inspect(req.jwtPayload));
+    return res.status(503).json({ error: 'Can not write reviews without autnehtication.' });
+  }
+
+  try{
+    connection = await oracledb.getConnection({
+      user  : userDB,
+      password  : passwordDB,
+      connectionString  : 'localhost/orclpdb1'
+    });
+    console.log("Succesfully connected to Oracle!!");
+    if(isbn && title && rating && comment){
+      str = "INSERT INTO RATING (Rating, Review, Book_id, Account_id) "
+            + "VALUES("+rating+" , '"+comment+"', '"+isbn+"', '"+req.jwtPayload.username+"') ";
+      await connection.execute(str);
+      connection.commit();
+      console.log("add 1 comment:",req.jwtPayload.username);
+    }
+    isbn = "";
+    title = "";
+    rating = "";
+    comment = "";
+
+    str = "select book.isbn, book.title, rating.rating, rating.review, account.name "
+          + "from rating, book, account "
+          + "where rating.book_id = book.isbn "
+          + "and account.id = rating.account_id "
+          + "and account.id = '"+req.jwtPayload.username+"'";
+    const qr = await connection.execute(str);
+    const count = qr.rows.length;
+    let result = '<table>';
+    for( let i = 0; i < count; i ++) {
+      result = result + '<tr>';
+      result = result + '<td><a href="https://isbnsearch.org/isbn/' + qr.rows[i][0] + '">'+qr.rows[i][0] + '</a></td>';
+      result = result + '<td>' + qr.rows[i][1] +'</td>';
+      result = result + '<td>' + qr.rows[i][2] +'</td>';
+      result = result + '<td>' + qr.rows[i][3] +'</td>';
+      result = result + '<td>' + qr.rows[i][4] +'</td>';
+      result = result + '<td><button>modify</button></td>';
+      result = result + '<td><button>delete</button></td>';
+      result = result + '</tr>';
+    }
+    result = result + '</table>';
+
+    res.render( 'reviewUser', { result: result, count: count } );
+
+  } catch(err){
+    console.log("Oracle connection Error: ", err);
+  } finally{
+    if(connection){
+      try{
+        await connection.close();
+      } catch(err){
+        console.log(err);
+      }
+    }
+  }
+})
+
+router.use('/reviewWrite', authenticator);
+router.get('/reviewWrite', async (req,res,next) => {
   let str;
   // connect Oracle Database
   async function fun(){
@@ -148,80 +257,16 @@ router.get('/reviewUser',(req,res,next) => {
         connectionString  : 'localhost/orclpdb1'
       });
       console.log("Succesfully connected to Oracle!!");
-      if(isbn && title && rating && comment){
-        str = "INSERT INTO RATING (Rating, Review, Book_id, Account_id) "
-             + "VALUES("+rating+" , '"+comment+"', '"+isbn+"', '"+userID+"') ";
-        await connection.execute(str);
-        connection.commit();
-        console.log("add 1 comment:",userID);
-      }
-      isbn = "";
-      title = "";
-      rating = "";
-      comment = "";
-
-      str = "select book.isbn, book.title, rating.rating, rating.review, account.name "
-            + "from rating, book, account "
-            + "where rating.book_id = book.isbn "
-            + "and account.id = rating.account_id "
-            + "and account.id = '"+userID+"'";
-      const qr = await connection.execute(str);
-      const count = qr.rows.length;
-      let result = '<table>';
-      for( let i = 0; i < count; i ++) {
-        result = result + '<tr>';
-        result = result + '<td><a href="https://isbnsearch.org/isbn/' + qr.rows[i][0] + '">'+qr.rows[i][0] + '</a></td>';
-        result = result + '<td>' + qr.rows[i][1] +'</td>';
-        result = result + '<td>' + qr.rows[i][2] +'</td>';
-        result = result + '<td>' + qr.rows[i][3] +'</td>';
-        result = result + '<td>' + qr.rows[i][4] +'</td>';
-        result = result + '<td><button>modify</button></td>';
-        result = result + '<td><button>delete</button></td>';
-        result = result + '</tr>';
-      }
-      result = result + '</table>';
-
-      res.render( 'reviewUser', { result: result, count: count } );
-
-    } catch(err){
-      console.log("Oracle connection Error: ", err);
-    } finally{
-      if(connection){
-        try{
-          await connection.close();
-        } catch(err){
-          console.log(err);
-        }
-      }
-    }
-  }
-
-  fun();  
-})
-/* write review 
-router.get('/reviewWrite',(req,res,next) => {
-  let str;
-  // connect Oracle Database
-  async function fun(){    
-    var isbn = req.query.bookISBN;
-    var title = req.query.bookTitle;
-    var rating = req.query.bookRating;
-    var comment = req.query.bookComment;
-
-    try{
-      connection = await oracledb.getConnection({
-        user  : userDB,
-        password  : passwordDB,
-        connectionString  : 'localhost/orclpdb1'
-      });
-      console.log("Succesfully connected to Oracle!!");
       console.log(isbn,title,rating,comment)
       if(isbn && title && rating && comment){
+        if (!req.jwtPayload || Object.entries(req.jwtPayload).length == 0) {
+          return res.status(503).json({ error: 'Can not write reviews without autnehtication.' });
+        }
         str = "INSERT INTO RATING (Rating, Review, Book_id, Account_id) "
-             + "VALUES("+rating+" , '"+comment+"', '"+isbn+"', '"+userID+"') ";
+             + "VALUES("+rating+" , '"+comment+"', '"+isbn+"', '"+req.jwtPayload.username+"') ";
         await connection.execute(str);
         connection.commit();
-        console.log("add 1 comment:",userID);
+        console.log("add 1 comment:",req.jwtPayload.username);
       }
 
       res.render( 'reviewWrite' );
@@ -239,9 +284,9 @@ router.get('/reviewWrite',(req,res,next) => {
     }
   }
 
-  fun();  
+  fun();
 })
-*/
+
 router.get('/reviewWrite',(req, res, next) => {
   res.render('reviewWrite')
 })
@@ -300,11 +345,12 @@ router.get('/ratingRank',(req,res,next) => {
   fun();
 })
 
+router.use('/comment', authenticator);
 router.get('/comment',(req,res,next) => {
   let str;
   // connect Oracle Database
   async function fun(){
-    
+
     var isbn = req.query.bookISBN;
     var title = req.query.bookTitle;
     var rating = req.query.bookRating;
@@ -321,10 +367,10 @@ router.get('/comment',(req,res,next) => {
         console.log("touch comment1: ",rating);
         console.log("touch comment2: ",comment);
         console.log("touch comment3: ",isbn);
-        console.log("touch comment4: ",userID);
-        
+        console.log("touch comment4: ",req.jwtPayload.username);
+
         str = "INSERT INTO RATING (Rating, Review, Book_id, Account_id) "
-              + "VALUES("+rating+" , '"+comment+"', '"+isbn+"', '"+userID+"') ";
+              + "VALUES("+rating+" , '"+comment+"', '"+isbn+"', '"+req.jwtPayload.username+"') ";
         await connection.execute(str);
         rating = "";
         comment="";
@@ -337,7 +383,7 @@ router.get('/comment',(req,res,next) => {
         // console.log("touch comment1: ",req.query.bookTitle);
 
         connection.commit();
-        console.log("add 1 comment:",userID);
+        console.log("add 1 comment:",req.jwtPayload.username);
       }
       str = "select book.isbn, book.title, rating.rating, rating.review, account.name "
             + "from rating, book, account "
@@ -372,7 +418,7 @@ router.get('/comment',(req,res,next) => {
     }
   }
 
-  fun();  
+  fun();
 })
 
 
